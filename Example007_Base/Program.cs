8b1c38045c398ea4bcd626d47b79bab86a5ccda7
Vladislav <89006566910@mail.ru>
2021-11-30 20:32:54 +0300

    Example007_Bases

---
+++ Example007_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано: пять гирь с разным весом в случайном порядке.
+// Найти: вес самой тяжелой гири.
+
+int a = 24;
+int b = 16;
+int c = 4;
+int d = 7;
+int x = 32;
+
+int max = a;
+// if (a > max) max = a; лишняя строка кода т.к уже max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (x > max) max = x;
+
+Console.Write("max = ");
+Console.WriteLine(max);
